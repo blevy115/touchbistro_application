cbc02de4492cb1a9d0bff78d5233946d9a34fbe0
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-01 18:54:13 -0500

    Installed axios, deleted initial app component and connected to express api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import connect from './axios/connect';
 import './App.css';
 
-function App() {
+const App = () =>  {
+  const mathAPI = async () => {
+      try {
+        const response = await connect.get('/testAPI')
+        setPhrase(response.data);
+      } catch (err) {
+        setPhrase('something went wrong');
+      }
+    }
+
+  const [phrase, setPhrase] = useState('');
+  useEffect(() => {
+    mathAPI();
+    setPhrase('hello World')
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{phrase}</p>
     </div>
   );
 }
