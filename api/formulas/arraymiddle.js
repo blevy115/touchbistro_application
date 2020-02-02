1c1e6f536be6969a9d522a570de46e4f36559855
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-01 23:21:11 -0500

    refactored middlearray function

--- api/formulas/arraymiddle.js
+++ api/formulas/arraymiddle.js
@@ -1,9 +1,7 @@
 const arraymiddle = (array) => {
-  if (array.length % 2 === 0) {
-    return `${array[Math.floor((array.length - 1) / 2)]} and ${array[Math.floor(array.length / 2)]}`
-  } else {
-    return array[Math.floor(array.length / 2)];
-  }
+  return array.length % 2 === 0 ?
+    `${array[Math.floor((array.length - 1) / 2)]} and ${array[Math.floor(array.length / 2)]}` :
+    array[Math.floor(array.length / 2)];
 }
 
 module.exports = arraymiddle;
