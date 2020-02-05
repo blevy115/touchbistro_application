c511433e842a407a04c442208fd4dbe5d00d3b2e
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-05 01:16:34 -0500

    Cleaned up component and added more styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,24 +8,24 @@ function App(props) {
         if (number > 100000000) {
           setPhrase('Number too large, try a smaller number');
           return;
-        }
+        };
         if (number > 1000000) setPhrase("Calculating...");
         const response = await connect.get('/findMedianPrime', {
           params: {
             number
           }
-        })
-        if (response.data.error){
+        });
+        if (response.data.error) {
           setPhrase(response.data.error);
           setResult('');
         } else {
           setResult(response.data.result);
           setPhrase("There's the Median Prime, Enter another number to try again.");
-        }
+        };
       } catch (err) {
         setPhrase('Something went wrong');
-      }
-    }
+      };
+    };
 
   const [phrase, setPhrase] = useState('');
   const [number, setNumber] = useState('');
@@ -33,14 +33,14 @@ function App(props) {
 
   useEffect(() => {
     setPhrase('Welcome, Please Enter a number, we will find the Median Prime Number less than it.')
-  }, [])
+  }, []);
 
   return (
     <div className="App">
       <p aria-label="phrase">{phrase}</p>
       <form onSubmit={(event) => {
         event.preventDefault()
-        mathAPI(number)
+        mathAPI(number);
       }}>
         <input
           placeholder="Enter Number"
@@ -48,18 +48,19 @@ function App(props) {
           name="number"
           aria-label="number"
           value={number}
+          autocomplete="off"
           onChange={inputField=> setNumber(inputField.target.value)}
         />
         <button
           type="submit"
           name="submit"
           aria-label="submit"
-          disabled={!number}
+          disabled={!number || phrase === 'Calculating...'}
         >Find My Prime</button>
       </form>
-      { result ? <p aria-label="result">{result}</p> : null }
+      { result ? <p id="result" aria-label="result">Result: {result}</p> : null }
     </div>
   );
-}
+};
 
 export default App;
