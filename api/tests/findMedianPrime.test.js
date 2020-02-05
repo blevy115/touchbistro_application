da428a67ea3d02b39b2b960bc80a3bf0cf455c59
Brandon Levy <brandon.levy@mail.mcgill.ca>
2020-02-05 01:18:16 -0500

    Added Jest for testing and wrote tests for Back-End

---
+++ api/tests/findMedianPrime.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('../app');
+
+describe('Succesful Data return', () => {
+  it('Should return a number as a result if succesful', async () => {
+    const res = await request(app)
+      .get('/findMedianPrime?number=2200');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('result');
+    expect(res.body.result).toBe(971);
+  });
+});
+
+describe('Unsuccesful Data return', () => {
+  it('Should return an error if less than 2', async () => {
+    const res = await request(app)
+      .get('/findMedianPrime?number=-30');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('error');
+      expect(res.body.error).toBe('Please Enter a number greater than 2');
+  });
+
+  it('Should return an error if not an integer', async () => {
+    const res = await request(app)
+      .get('/findMedianPrime?number=24.76');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body).toHaveProperty('error');
+      expect(res.body.error).toBe('Please Enter an Integer');
+  });
+});
+
+describe('Unseccesful pathway', () => {
+  it('Should return 404 if not correct path', async () => {
+    const res = await request(app)
+      .get('/find?number=30');
+      expect(res.statusCode).toEqual(404);
+  });
+});
